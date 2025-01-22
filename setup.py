b242a7a75c9f53d62aaa1c56b99b04b17bb0da4e
ParveenUppar <144151782+PraveenUppar@users.noreply.github.com>
2025-01-22 12:39:13 +0530

    NLP

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Praveen Uppar",
+    author_email="paviiuppar@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
